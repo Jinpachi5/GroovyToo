65c8bb180deeb78c3f42618ee8f261cc301f513c
Jinpachi5 <alesandercampbell1@gmail.com>
2022-02-18 22:56:00 -0500

    ah

--- cogs/youtube.py
+++ cogs/youtube.py
@@ -1,10 +1,55 @@
 from tempfile import _TemporaryFileWrapper
 import discord
 from discord.ext import commands
+import asyncio
 import os
+import youtube_dl
 
 
+youtube_dl.utils.bug_reports_message = lambda: ''
 
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+queue = []
 activity = discord.Activity(type = discord.ActivityType.listening, name = "a song")
 class Youtube(commands.Cog):
     def __init__(self, client):
@@ -19,7 +64,13 @@ class Youtube(commands.Cog):
 
     @commands.command()
     async def play(self, ctx, url : str):
+        global queue
+        queue.append(url)
         voice = discord.utils.get(self.client.voice_clients, guild = ctx.guild)
+        
+        if voice.is_playing():
+            await ctx.send('Song has been added to queue')
+
         if ctx.author.voice is None:
             await ctx.send('You are not connected to a voice channel')
         channel = ctx.author.voice.channel
@@ -27,6 +78,13 @@ class Youtube(commands.Cog):
             await channel.connect()
         else: 
             await voice.move_to(channel)
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(queue[0], loop=self.client.loop)
+            voice.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+        await ctx.send('**Now playing:** {}'.format(player.title))
+        del(queue[0])
 
     @commands.command()
     async def leave(self, ctx):
@@ -61,6 +119,21 @@ class Youtube(commands.Cog):
         #voice = voiceClient
         voice = discord.utils.get(self.client.voice_clients, guild = ctx.guild)
         voice.stop()
+
+    @commands.command()
+    async def queue(self, ctx):
+        await ctx.send(f'Your queue is now `{queue}!`')
+
+    @commands.command()
+    async def remove(ctx, number):
+        global queue
+
+        try:
+            del(queue[int(number)])
+            await ctx.send(f'Your queue is now `{queue}!`')
+    
+        except:
+            await ctx.send('Your queue is either **empty** or the index is **out of range**')
         
 
 
